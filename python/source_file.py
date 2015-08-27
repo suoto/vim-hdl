649e0c89d687ee2abb8e9d349676fc76f0bbd76b
andre820 <andre820@gmail.com>
2015-08-27 17:46:11 -0300

    Adding methods to get library dependency. This should be used to give some hint to order the sources to be compiled

--- python/source_file.py
+++ python/source_file.py
@@ -15,38 +15,71 @@
 
 import re, os
 
-RE_IS_PACKAGE = re.compile(r"^\s*package\s+\w+\s+is\b|^\s*package\s+body\s+\w+\s+is\b", flags=re.I)
+_RE_IS_PACKAGE = re.compile(
+    r"^\s*package\s+\w+\s+is\b|^\s*package\s+body\s+\w+\s+is\b",
+    flags=re.I)
+
+_RE_LIBRARY_DECLARATION = re.compile(r"^\s*library\s.*", flags=re.I)
+_RE_LIBRARY_EXTRACT = re.compile(r"^\s*library\s+|\s*;.*", flags=re.I)
+
+_RE_USE_CLAUSE = re.compile(r"^\s*use\s+[\w\.]+.*", flags=re.I)
+_RE_USE_EXTRACT = re.compile(r"^\s*use\s+|\s*;.*", flags=re.I)
+
+# FIXME: Built-in libraries should be defined via Vim configuration interface
+# and thus be in a specific Python package from which we should import
+BUILTIN_LIBRARIES = ('ieee', 'std', 'altera', 'modelsim_lib', 'unisim',
+                     'xilinxcorelib', 'synplify', 'synopsis', 'altera_mf')
 
 class VhdlSourceFile(object):
     def __init__(self, filename):
         self.filename = filename
-        self._isPkg = None
+        self._is_package = None
+        self._package_name = None
 
-    def __getstate__(self):
-        state = {'filename' : self.filename,
-                '_isPkg' : self._isPkg}
-        return state
-    def __setstate__(self, d):
-        self.__dict__.update(d)
     def __getattr__(self, attr):
-        if hasattr(self.filename, attr):
+        if hasattr(str, attr):
             return getattr(self.filename, attr)
+        raise AttributeError()
 
     def isPackage(self):
-        if self._isPkg is None:
-        #  if True:
-            self._isPkg = False
+        if self._is_package is None:
+            self._is_package = False
             for l in open(self.filename, 'r').read().split('\n'):
-                if RE_IS_PACKAGE.match(l):
-                    self._isPkg = True
+                if _RE_IS_PACKAGE.match(l):
+                    self._is_package = True
                     break
-        return self._isPkg
+        return self._is_package
+
+    def getPackageName(self):
+        if self._package_name is None:
+            for l in open(self.filename, 'r').read().split('\n'):
+                if _RE_IS_PACKAGE.match(l):
+                    self._package_name = \
+                            re.sub(r"^\s*package\s+|\s+is.*$", "", l)
+                    break
+        return self._package_name
+
+    def getDependencies(self):
+        libs = []
+        packages = []
+        for l in open(self.filename, 'r').read().split('\n'):
+            l = re.sub(r"\s*--.*", "", l)
+            if _RE_LIBRARY_DECLARATION.match(l):
+                lib = _RE_LIBRARY_EXTRACT.sub("", l)
+                if lib not in BUILTIN_LIBRARIES:
+                    libs.append(lib)
+            if _RE_USE_CLAUSE.match(l):
+                lib, package = _RE_USE_EXTRACT.sub("", l).split('.')[:2]
+                if lib not in BUILTIN_LIBRARIES:
+                    packages.append((lib, package))
+        return libs, packages
 
     def __str__(self):
         return str(self.filename)
 
     def getmtime(self):
         return os.path.getmtime(self.filename)
+
     def abspath(self):
         return os.path.abspath(self.filename)
 
