04082fb55480f45e0ffd4d52928528aa9852ba30
suoto <andre820@gmail.com>
2016-03-21 20:15:33 -0300

    Fixing Vim master branch test

--- .ci/setup_vim.sh
+++ .ci/setup_vim.sh
@@ -4,25 +4,26 @@ set -x
 
 # Clone repo if the cached folder doesn't exists
 if [ ! -d "${CACHE}/vim-${VIM_VERSION}" ]; then
-  git clone https://github.com/vim/vim ${CACHE}/vim-${VIM_VERSION}
+  git clone https://github.com/vim/vim "${CACHE}/vim-${VIM_VERSION}"
 fi
 
 
-if [ ! -f "${CACHE}/vim-${VIM_VERSION}/src/vim" ]; then
-  cd ${CACHE}/vim-${VIM_VERSION}
+if [ ! -f "${CACHE}/vim-${VIM_VERSION}/src/vim" -o "${VIM_VERSION}" == "master" ]; then
+  cd "${CACHE}/vim-${VIM_VERSION}"
   if [ "${VIM_VERSION}" == "master" ]; then
     # If we're testing the latest Vim version, we only pull the latest changes
+    git clean -fdx
     git checkout master
     git pull
   else
-    git checkout ${VIM_VERSION}
+    git checkout "${VIM_VERSION}"
   fi
   ./configure --with-features=huge --enable-pythoninterp --enable-gui=auto --with-x
 
   make all -j4
 fi
 
-cd ${TRAVIS_BUILD_DIR}
+cd "${TRAVIS_BUILD_DIR}"
 
 $VIM_BINARY --version
 
